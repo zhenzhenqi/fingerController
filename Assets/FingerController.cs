5b1d436daa2b3fc6763ccc762fa9feae112a7bf6
jane363636 <jane363636@gmail.com>
2017-07-20 07:21:46 +0800

    first commit

---
+++ Assets/FingerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerController : MonoBehaviour {
+
+	Animator fingerAnimator;
+
+	// Use this for initialization
+	void Start () {
+		fingerAnimator = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		fingerAnimator.SetFloat ("speed",  Mathf.Abs((  Input.GetAxis ("Vertical") + Mathf.Abs (Input.GetAxis ("Horizontal")) ))*1.2f, 0.2f, Time.deltaTime);
+
+
+
+	
+
+		
+	}
+}
